36c5d1a0a9a8828d027cb0650ff6cdf19b109e25
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-11-21 07:23:41 +0530

    ADDED: Welcome to Employee Payroll Service Data Base

---
+++ src/main/java/com/employeePayroll/EmployeePayrollServiceDB.java
@@ -0,0 +1,10 @@
+package com.employeePayroll;
+
+public class EmployeePayrollServiceDB {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Payroll DataBase");
+	}
+
+}
